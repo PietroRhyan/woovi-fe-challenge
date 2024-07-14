2085f998cdf8cf9650837adb5ea331faa6de8545
Pietro Rhyan <pietroqjg@gmail.com>
2024-07-14 16:56:44 -0300

    fix: radio group

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -28,7 +28,7 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-[26px] w-[26px] rounded-full border border-gray ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "absolute top-5 right-5 aspect-square h-[26px] w-[26px] rounded-full border border-gray ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
